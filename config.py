0047e435f62fc51a61f6226032e15c4049859b32
Vladislav <142813841+VladislavSlusarenko@users.noreply.github.com>
2024-11-13 23:56:54 +0200

    Update config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_TOKEN = '7338566190:AAGtOOMI8StYiU5HZ2vrkWY12QtIR6iL1n4'  # Укажите свой токен
+CHANNEL_ID = '-1002302094356'  # Укажите ID своего канала
+ADMIN_ID = [922094773, 1166978466]  # Укажите user_id администратора, теперь это список
+
+print(API_TOKEN, CHANNEL_ID, ADMIN_ID)
